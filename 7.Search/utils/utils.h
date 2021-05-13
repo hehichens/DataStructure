808c73a243e635ecd7f3ba05475ba96c6cca6bd9
hehichens <hichens@qq.com>
2021-05-13 22:30:13 +0800

    :fire:update

---
+++ 7.Search/utils/utils.h
@@ -0,0 +1,21 @@
+/*
+utils
+edit by hichens
+*/
+
+
+#ifndef __SEARCH__UTILS__C__
+#define __SEARCH__UTILS__C__
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#define INFINITY 0x7fffffff
+#define bool short
+#define true 1
+#define false 0
+
+
+
+
+#endif
